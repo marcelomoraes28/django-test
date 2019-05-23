3ec6b4e17c0c220c0028b71eb1090e393b321589
Marcelo Moraes <marcelomoraesjr28@gmail.com>
2019-05-22 23:35:10 -0300

    Set password in user

--- happiness/level/tests/base.py
+++ happiness/level/tests/base.py
@@ -5,3 +5,5 @@ from level.factories import UserFactory
 class BaseTestCase(TestCase):
     def setUp(self):
         self.user = UserFactory()
+        self.user.set_password('admin123-foo-bar')
+        self.user.save()
